15c0bec12d78463a6759c1e0ed7bbfbbaca99070
Angel Torres <alas4224@gmail.com>
2019-01-11 11:51:51 -0600

    finished displaying all data for MVP

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,15 +3,20 @@ import './StarWars.css';
 
 const Character = (props) => {
     return (
-        <div>
+        <div className="character-container">
             <h1>Name: {props.character.name}</h1>
-            <p>Birth Year: {props.character.birth_year}</p>
-            <p>Eye Color: {props.character.eye_color}</p>
-            <p>Gender: {props.character.gender}</p>
-            <p>Hair Color: {props.character.hair_color}</p>
-            <p>Heigth: {props.character.height}</p>
-            <p>Mass: {props.character.mass}</p>
-            <p>Skin Color: {props.character.skin_color}</p>
+            <div className="attribute"><h2>Birth Year:</h2> {props.character.birth_year}</div>
+            <div className="attribute"><h2>Eye Color:</h2> {props.character.eye_color}</div>
+            <div className="attribute"><h2>Gender:</h2> {props.character.gender}</div>
+            <div className="attribute"><h2>Hair Color:</h2> {props.character.hair_color}</div>
+            <div className="attribute"><h2>Heigth:</h2> {props.character.height}</div>
+            <div className="attribute"><h2>Mass:</h2> {props.character.mass}</div>
+            <div className="attribute"><h2>Skin Color:</h2> {props.character.skin_color}</div>
+            <div className="films attribute"><h2>Films:</h2> {props.character.films.map(film => {
+                return (
+                    <p className="film">{film}</p>
+                )
+            })}</div>
         </div>
     )
 }
